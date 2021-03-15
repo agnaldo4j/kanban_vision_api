71e6c23a618ff4b2cff33a81d1f1699d6cd76741
Agnaldo de Oliveira <agnaldo4j@gmail.com>
2021-03-14 21:21:05 -0300

    Retrieve organization by name

--- rest/src/main/scala/com/kanban/vision/rest/RestApiOrganization.scala
+++ rest/src/main/scala/com/kanban/vision/rest/RestApiOrganization.scala
@@ -7,28 +7,30 @@ import com.kanban.vision.domain.Organization
 import com.kanban.vision.eventbus.EventBus
 import com.kanban.vision.rest.Main.{get, path}
 import com.kanban.vision.rest.presentation.Presentation._
+import com.kanban.vision.usecase.exceptions.OrganizationAlreadyExists
 import io.finch.Endpoint.post
 import io.finch.catsEffect.jsonBody
-import io.finch.{BadRequest, Endpoint, InternalServerError, Ok}
+import io.finch.{BadRequest, Conflict, Endpoint, InternalServerError, Ok}
 import io.finch.circe._
 
 import scala.util.{Failure, Success}
 
 object RestApiOrganization {
+  def getOrganizations(eventBus: EventBus): Endpoint[IO, List[Organization]] = get("organizations") {
+    eventBus.execute[List[Organization]](GetAllOrganizationsFromSystem()) match {
+      case Success(result) => Ok(result)
+      case Failure(_) => Ok(List.empty)
+    }
+  }
+
   def addOrganization(eventBus: EventBus): Endpoint[IO, AddedOrganization] = post(
     "organizations" :: jsonBody[NewOrganization]
   ) { newOrganization: NewOrganization =>
     eventBus.execute[Organization](AddOrganizationOnSystem(newOrganization.name)) match {
       case Success((_, organization)) => Ok(AddedOrganization(organization))
+      case Failure(ex: OrganizationAlreadyExists) => Conflict(ex)
       case Failure(ex: Exception) => BadRequest(ex)
       case Failure(ex) => InternalServerError(new IllegalStateException(ex))
-    }
-  }
-
-  def getOrganizations(eventBus: EventBus): Endpoint[IO, List[Organization]] = get("organizations") {
-    eventBus.execute[List[Organization]](GetAllOrganizationsFromSystem()) match {
-      case Success(result) => Ok(result)
-      case Failure(_) => Ok(List.empty)
     }
   }
 
--- usecase/src/main/scala/com/kanban/vision/usecase/system/Changeable.scala
+++ usecase/src/main/scala/com/kanban/vision/usecase/system/Changeable.scala
@@ -2,6 +2,7 @@ package com.kanban.vision.usecase.system
 
 import com.kanban.vision.domain.Organization
 import com.kanban.vision.domain.SystemChangeable.{AddOrganization, DeleteOrganization, SystemCommand}
+import com.kanban.vision.usecase.exceptions.OrganizationAlreadyExists
 
 import scala.util.{Failure, Success, Try}
 
@@ -9,7 +10,10 @@ trait Changeable {
   def execute[RETURN](command: SystemCommand): Try[RETURN] = {
     command match {
       case AddOrganization(name, kanbanSystem) =>
-        Success(kanbanSystem.addOrganization(Organization(name = name)).asInstanceOf[RETURN])
+        kanbanSystem.organizationByName(name) match {
+          case Some(_) => Failure(new OrganizationAlreadyExists(name))
+          case None => Success(kanbanSystem.addOrganization(Organization(name = name)).asInstanceOf[RETURN])
+        }
       case DeleteOrganization(id, kanbanSystem) =>
         Success(kanbanSystem.removeOrganization(id).asInstanceOf[RETURN])
       case _ => Failure(new IllegalStateException(s"Command not found ${command}"))
